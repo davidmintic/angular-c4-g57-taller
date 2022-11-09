49e54a088eeafd70dedb017425062a93bef2f988
davidmintic <90147680+davidmintic@users.noreply.github.com>
2022-11-08 22:23:30 -0500

    funiones nuevas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CrudUsuariosComponent } from './crud-usuarios/crud-usuarios.component';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
-import { CrudVehiculosComponent } from './crud-vehiculos/crud-vehiculos.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzSelectModule } from 'ng-zorro-antd/select';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CrudUsuariosComponent } from "./crud-usuarios/crud-usuarios.component";
+import { NzTableModule } from "ng-zorro-antd/table";
+import { NzDividerModule } from "ng-zorro-antd/divider";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzBreadCrumbModule } from "ng-zorro-antd/breadcrumb";
+import { CrudVehiculosComponent } from "./crud-vehiculos/crud-vehiculos.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { NzSelectModule } from "ng-zorro-antd/select";
+import { DialogInfoComponent } from "./dialog-info/dialog-info.component";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [AppComponent, CrudUsuariosComponent, CrudVehiculosComponent],
+  declarations: [
+    AppComponent,
+    CrudUsuariosComponent,
+    CrudVehiculosComponent,
+    DialogInfoComponent,
+  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NzTableModule,
     NzDividerModule,
@@ -30,6 +39,7 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
     NzIconModule,
     NzSelectModule,
     ReactiveFormsModule,
+    NzModalModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
